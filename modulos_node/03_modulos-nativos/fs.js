b238b90d6e4055ddda39daa56b9d10476db1b083
tiago19deoliveira <tiago18deoliveira@gmail.com>
2025-02-09 14:08:19 -0300

    feat: finalização de módulo fs e aprendizado do módulo crypto

--- modulos_node/03_modulos-nativos/fs.js
+++ modulos_node/03_modulos-nativos/fs.js
@@ -1,7 +1,13 @@
+const { error } = require("console");
 const fs = require("fs");
 const path = require("path");
-fs.mkdir(path.join(__dirname, "new-folder"), () => {});
 
+//criando pasta
+fs.mkdir(path.join(__dirname, "new-folder"), (error) => {
+  console.log(error);
+});
+
+// criando arquivo na pasta
 fs.writeFile(
   path.join(__dirname, "new-folder", "new-file.txt"),
   "Bem vindo ao fsc",
@@ -10,4 +16,25 @@ fs.writeFile(
   }
 );
 
-fs.appendFile(path.join(__dirname, "new-folder", "new-file.txt"));
+// fs.writeFile(path.join(__dirname, "03_modulos-nativos", "crypto.js"));
+
+// adicionando contéudo ao arquivo
+fs.appendFile(
+  path.join(__dirname, "new-folder", "new-file.txt"),
+  "Hello  my brother",
+  (error) => {
+    console.log(error);
+  }
+);
+
+//ler arquivo
+
+let contentFile = "oi";
+fs.readFile(
+  path.join(__dirname, "new-folder, new-file.txt"),
+  "utf-8",
+  (error, data) => {
+    contentFile = data;
+    console.log("Conteudo do data", data);
+  }
+);
