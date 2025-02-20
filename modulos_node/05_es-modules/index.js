8147b3dc353aaa26c89a8ee9e22676382e3b40f1
tiago19deoliveira <tiago18deoliveira@gmail.com>
2025-02-20 00:04:43 -0300

    testando formas de importar todo o conteúdo do modulo

--- modulos_node/05_es-modules/index.js
+++ modulos_node/05_es-modules/index.js
@@ -1,10 +1,12 @@
 // const { createProduct, Product } = require("./products");
 // import { createProduct, Product } from "./products.js";
 //importando de módulo não compatível com es import export
+import productPrice, { Product, createProduct } from "./products.js";
 import lodash from "lodash";
 const { filter } = lodash;
-
-import productPrice, { Product, createProduct } from "./products.js";
+//para importar tudo que existe em outro modulo e ter acesso como uma propriedade
+import * as productModule from "./products.js";
+const moduleOfProduct = productModule.Product;
 
 const product = createProduct("2", "kenga", "idont n", productPrice);
 
